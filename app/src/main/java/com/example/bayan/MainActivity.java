ca7ba30de4642121eebdcdbce2516e2086f1712c
bayanbamb <bayanbamb@yahoo.com>
2023-03-21 15:31:34 +0300

    Initial commit

---
+++ app/src/main/java/com/example/bayan/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.bayan;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
